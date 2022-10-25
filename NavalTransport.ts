e74462c2cc3be872c930e2492a49741bba7f65cf
dom111 <dom111@users.noreply.github.com>
2022-10-25 20:02:22 +0100

    Update to use latest `core-unit-transport` changes.

--- NavalTransport.ts
+++ NavalTransport.ts
@@ -7,81 +7,23 @@ import {
   instance as transportRegistryInstance,
 } from '@civ-clone/core-unit-transport/TransportRegistry';
 import City from '@civ-clone/core-city/City';
-import { ITransport } from '@civ-clone/core-unit-transport/Transport';
 import Naval from '@civ-clone/base-unit-type-naval/Naval';
 import Player from '@civ-clone/core-player/Player';
-import Sleep from '@civ-clone/base-unit-action-sleep/Sleep';
 import Tile from '@civ-clone/core-world/Tile';
-import TransportManifest from '@civ-clone/core-unit-transport/TransportManifest';
-import Unit from '@civ-clone/core-unit/Unit';
+import Transport from '@civ-clone/core-unit-transport/Transport';
 
-export interface INavalTransport extends ITransport {}
-
-export class NavalTransport extends Naval implements INavalTransport {
-  #transportRegistry: TransportRegistry;
-
+export class NavalTransport extends Transport(Naval) {
   constructor(
     city: City | null,
     player: Player,
     tile: Tile,
-    rulesRegistry: RuleRegistry = ruleRegistryInstance,
+    ruleRegistry: RuleRegistry = ruleRegistryInstance,
     transportRegistry: TransportRegistry = transportRegistryInstance
   ) {
-    super(city, player, tile, rulesRegistry);
+    super(city, player, tile, ruleRegistry);
 
-    this.#transportRegistry = transportRegistry;
-  }
-
-  canStow(unit: Unit): boolean {
-    return !this.cargo().includes(unit);
-  }
-
-  capacity(): number {
-    return 0;
-  }
-
-  cargo(): Unit[] {
-    return this.#transportRegistry
-      .getByTransport(<Unit>this)
-      .map((manifest: TransportManifest): Unit => manifest.unit());
-  }
-
-  hasCapacity(): boolean {
-    return (
-      this.#transportRegistry.getByTransport(<Unit>this).length <
-      this.capacity()
-    );
-  }
-
-  hasCargo(): boolean {
-    return this.#transportRegistry.getByTransport(<Unit>this).length > 0;
-  }
-
-  stow(unit: Unit) {
-    if (!this.hasCapacity() || !this.canStow(unit)) {
-      return false;
-    }
-
-    this.#transportRegistry.register(
-      new TransportManifest(this as ITransport, unit)
-    );
-
-    // TODO: use a rule to trigger this?
-    unit.action(new Sleep(unit.tile(), unit.tile(), unit));
-
-    return true;
-  }
-
-  unload(unit: Unit) {
-    try {
-      const manifest = this.#transportRegistry.getByUnit(unit);
-
-      this.#transportRegistry.unregister(manifest);
-
-      return true;
-    } catch (e) {
-      return false;
-    }
+    this.setRuleRegistry(ruleRegistry);
+    this.setTransportRegistry(transportRegistry);
   }
 }
 
